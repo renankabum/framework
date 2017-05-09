99fd7fa736facf25c980f3ef1dc2174eb32282ec
Vagner Cardoso <vagnercardosoweb@gmail.com>
2017-05-09 15:31:15 -0300

    - Colocado funções para envio de imagem!

--- src/helpers.php
+++ src/helpers.php
@@ -173,6 +173,189 @@ if (!function_exists('view')) {
   }
 }
 
+if (!function_exists('imagem')) {
+  /**
+   * @param $src
+   * @param $dest
+   * @param $maxWidth
+   * @param $maxHeight
+   * @param $quality
+   *
+   * @return bool
+   */
+  function imagem($src, $dest, $maxWidth, $maxHeight, $quality)
+  {
+    $png = false;
+    if (file_exists($src) && isset($dest)) {
+      // Retorna informação sobre o path do um arquivo
+      $destInfo = pathinfo($dest);
+      // Retorna o tamanho da imagem
+      $srcSize = getimagesize($src);
+      
+      // tamanho de destino $destSize[0] = width, $destSize[1] = height
+      $srcRatio = $srcSize[0] / $srcSize[1]; // width/height média
+      $destRatio = $maxWidth / $maxHeight;
+      if ($destRatio > $srcRatio) {
+        $destSize[1] = $maxHeight;
+        $destSize[0] = $maxHeight * $srcRatio;
+      } else {
+        $destSize[0] = $maxWidth;
+        $destSize[1] = $maxWidth / $srcRatio;
+      }
+      // retifica o arquivo
+      if ($destInfo['extension'] == "gif") {
+        $dest = substr_replace($dest, 'jpg', -3);
+      }
+      
+      // cria uma imagem com a extensão original
+      switch ($srcSize[2]) {
+        case 1: //GIF
+          $srcImage = imagecreatefromgif($src);
+          break;
+        case 2: //JPEG
+          $srcImage = imagecreatefromjpeg($src);
+          break;
+        case 3: //PNG
+          $srcImage = imagecreatefrompng($src);
+          imagesavealpha($srcImage, true);
+          $png = true;
+          break;
+        default:
+          return false;
+          break;
+      }
+      
+      // ajusta a cor
+      if (function_exists("imagecreatetruecolor")) {
+        $destImage = imagecreatetruecolor($destSize[0], $destSize[1]);
+      } else {
+        $destImage = imagecreate($destSize[0], $destSize[1]);
+      }
+      
+      if (function_exists("imageantialias")) {
+        imageantialias($destImage, true);
+      }
+      
+      if ($png) {
+        if (substr($dest, -3) == 'png') {
+          imagealphablending($destImage, false);
+          imagesavealpha($destImage, true);
+          $transparent = imagecolorallocatealpha($destImage, 255, 255, 255, 127);
+        } else {
+          $white = imagecolorallocate($destImage, 255, 255, 255);
+          imagefilledrectangle($destImage, 0, 0, $destSize[0], $destSize[1], $white);
+        }
+      }
+      
+      // copia a figura redimencionando o seu tamanho
+      if (function_exists("imagecopyresampled")) {
+        imagecopyresampled($destImage, $srcImage, 0, 0, 0, 0, $destSize[0], $destSize[1], $srcSize[0], $srcSize[1]);
+      } else {
+        imagecopyresized($destImage, $srcImage, 0, 0, 0, 0, $destSize[0], $destSize[1], $srcSize[0], $srcSize[1]);
+      }
+      
+      if (substr($dest, -3) == 'png') {
+        imagepng($destImage, $dest);
+      } else {
+        imagejpeg($destImage, $dest, $quality);
+      }
+      
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+if (!function_exists('imagemTamExato')) {
+  /**
+   * @param $imgSrc
+   * @param $dest
+   * @param $thumbnail_width
+   * @param $thumbnail_height
+   * @param $quality
+   *
+   * @return bool
+   */
+  function imagemTamExato($imgSrc, $dest, $thumbnail_width, $thumbnail_height, $quality)
+  {
+    if (file_exists($imgSrc) && isset($dest)) {
+      $srcSize = getimagesize($imgSrc);
+      $destInfo = pathinfo($dest);
+      
+      // retifica o arquivo
+      if ($destInfo['extension'] == "gif") {
+        $dest = substr_replace($dest, 'jpg', -3);
+      }
+      
+      list($width_orig, $height_orig) = getimagesize($imgSrc);
+      
+      $png = false;
+      
+      switch ($srcSize[2]) {
+        case 1: //GIF
+          $myImage = imagecreatefromgif($imgSrc);
+          break;
+        case 2: //JPEG
+          $myImage = imagecreatefromjpeg($imgSrc);
+          break;
+        case 3: //PNG
+          $myImage = imagecreatefrompng($imgSrc);
+          $png = true;
+          break;
+        default:
+          return false;
+          break;
+      }
+      
+      $ratio_orig = $width_orig / $height_orig;
+      
+      if ($thumbnail_width / $thumbnail_height > $ratio_orig) {
+        $new_height = $thumbnail_width / $ratio_orig;
+        $new_width = $thumbnail_width;
+      } else {
+        $new_width = $thumbnail_height * $ratio_orig;
+        $new_height = $thumbnail_height;
+      }
+      
+      $x_mid = $new_width / 2;  //horizontal middle
+      $y_mid = $new_height / 2; //vertical middle
+      
+      $process = imagecreatetruecolor(round($new_width), round($new_height));
+      $thumb = imagecreatetruecolor($thumbnail_width, $thumbnail_height);
+      
+      if ($png) {
+        if (substr($dest, -3) == 'png') {
+          imagesavealpha($myImage, true);
+          imagealphablending($process, false);
+          imagesavealpha($process, true);
+          $transparent = imagecolorallocatealpha($process, 255, 255, 255, 127);
+          imagefilledrectangle($process, 0, 0, $new_width, $new_height, $transparent);
+          imagecopyresampled($process, $myImage, 0, 0, 0, 0, $new_width, $new_height, $width_orig, $height_orig);
+          $thumb = $process;
+        } else {
+          $white = imagecolorallocate($thumb, 255, 255, 255);
+          imagefilledrectangle($thumb, 0, 0, $thumbnail_width, $thumbnail_width, $white);
+          imagecopyresampled($thumb, $myImage, 0, 0, 0, 0, $new_width, $new_height, $width_orig, $height_orig);
+        }
+      } else {
+        imagecopyresampled($process, $myImage, 0, 0, 0, 0, $new_width, $new_height, $width_orig, $height_orig);
+        imagecopyresampled($thumb, $process, 0, 0, ($x_mid - ($thumbnail_width / 2)), ($y_mid - ($thumbnail_height / 2)), $thumbnail_width, $thumbnail_height, $thumbnail_width, $thumbnail_height);
+      }
+      
+      if (substr($dest, -3) == 'png') {
+        imagepng($thumb, $dest);
+      } else {
+        imagejpeg($thumb, $dest, $quality);
+      }
+      
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
 if (!function_exists('request')) {
   /**
    * Get instance request
