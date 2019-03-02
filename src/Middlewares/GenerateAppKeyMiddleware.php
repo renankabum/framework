22919f0b0a2a497348ead06f96e9393715d588f7
Vagner Cardoso <vagnercardosoweb@gmail.com>
2019-03-02 02:09:04 -0300

    - Geração das chaves privadas para `APP_KEY`, `DEPLOY_TOKEN`, 'API_TOKEN'.

--- src/Middlewares/GenerateAppKeyMiddleware.php
+++ src/Middlewares/GenerateAppKeyMiddleware.php
@@ -28,33 +28,89 @@ namespace Core\Middlewares {
         /**
          * Registra middleware para criação do APP_KEy
          *
-         * @param \Slim\Http\Request  $request  PSR7 request
+         * @param \Slim\Http\Request $request PSR7 request
          * @param \Slim\Http\Response $response PSR7 response
-         * @param callable            $next     Next middleware
+         * @param callable $next Next middleware
          *
          * @return \Slim\Http\Response
          */
         public function __invoke(Request $request, Response $response, callable $next)
         {
-            $originalAppKey = env('APP_KEY', null);
+            // Gera chaves secretas da aplicação caso não existem
+            $this->generateAppKey();
+            $this->generateDeployKey();
+            $this->generateApiBasicKey();
             
-            if (empty($originalAppKey)) {
-                $newAppKey = 'base64:'.Str::randomBytes(64);
-                $scaped = preg_quote("={$originalAppKey}", '/');
+            // Retorna a resposta
+            $response = $next($request, $response);
+            
+            return $response;
+        }
+        
+        /**
+         * Gera a chave da aplicação
+         */
+        protected function generateAppKey()
+        {
+            $originalKey = env('APP_KEY', null);
+            
+            if (empty($originalKey)) {
+                $key = 'base64:'.Str::randomBytes(64);
+                $scaped = preg_quote("={$originalKey}", '/');
                 
                 file_put_contents(
                     APP_FOLDER.'/.env',
                     preg_replace(
                         "/^APP_KEY{$scaped}/m",
-                        "APP_KEY={$newAppKey}",
+                        "APP_KEY={$key}",
                         file_get_contents(APP_FOLDER.'/.env')
                     )
                 );
             }
+        }
+        
+        /**
+         * Gera a chave para o deploy
+         */
+        protected function generateDeployKey()
+        {
+            $originalKey = env('DEPLOY_TOKEN', null);
             
-            $response = $next($request, $response);
+            if (empty($originalKey)) {
+                $key = Str::randomBytes(64);
+                $scaped = preg_quote("={$originalKey}", '/');
+                
+                file_put_contents(
+                    APP_FOLDER.'/.env',
+                    preg_replace(
+                        "/^DEPLOY_TOKEN{$scaped}/m",
+                        "DEPLOY_TOKEN={$key}",
+                        file_get_contents(APP_FOLDER.'/.env')
+                    )
+                );
+            }
+        }
+        
+        /**
+         * Gera a chave para a api
+         */
+        protected function generateApiBasicKey()
+        {
+            $originalKey = env('API_TOKEN', null);
             
-            return $response;
+            if (empty($originalKey)) {
+                $key = Str::randomBytes(64);
+                $scaped = preg_quote("={$originalKey}", '/');
+                
+                file_put_contents(
+                    APP_FOLDER.'/.env',
+                    preg_replace(
+                        "/^API_TOKEN{$scaped}/m",
+                        "API_TOKEN={$key}",
+                        file_get_contents(APP_FOLDER.'/.env')
+                    )
+                );
+            }
         }
     }
 }
